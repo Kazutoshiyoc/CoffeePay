5fe33e95478cf438f02a95dc054d13a5b1636ec1
KazutoshiYoshii <Kazutoshiyoc@gmail.com>
2021-02-12 21:26:05 +0900

    管理画面のフロントサイドを実装

--- server.js
+++ server.js
@@ -3,48 +3,89 @@ process.on('unhandledRejection', console.dir);
 const fs = require('fs');
 const http = require('http');
 
-// --------------------------------------------------
-//                    HTTPサーバ
-// --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+//                                                      HTTPサーバ
+// -----------------------------------------------------------------------------------------------------------------------
 const server = http.createServer(function(req, res) {
 
+    var thisStatusCode, html;
+    const InternalServerError = '<h1>CoffeePay</h1><hr><h2>500 InternalServerError</h2>';
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // '/'にGETアクセスされた場合 index.htmlを表示
 	if (req.url === '/' && req.method === 'GET') {
 
         // index.htmlを読み込む
-        fs.readFile(__dirname + '/index.html', {
+        fs.readFile(docRoot + 'index.html', {
             encoding: 'utf8'
-        }, function(err_404, html) {
-            // ファイルの読み込みに失敗したら404エラーのレスポンスを返す
-			if (err_404) {
-				fs.readFile(__dirname + '/error/404.html', {
+        }, function(err_404, index) {
+
+			if (err_404) {                                                  // index.htmlの読み込みに失敗した場合
+                fs.readFile(docRoot + '/error/404.html', {
 					encoding: 'utf8'
-				}, function(err_500, notFound) {
-					// 404ファイルの読み込みに失敗したら500エラーのレスポンスを返す*/
-					if (err_500) {
-						res.statusCode = 500;
-						res.end('<h1>CoffeePay for Nespresso</h1><hr><h2>500 InternalServerError</h2>');
-					} else {
-						res.setHeader('Content-Type', 'text/html');
-						res.end(notFound);
-					}
-				});
-			}
-			// ファイルの読み込みが成功したらHTMLを返す
-			else {
-				res.setHeader('Content-Type', 'text/html');
-				res.end(html);
-            } 
+				}, function(err_500, NotFound) {
+					if (err_500) [thisStatusCode, html] = [500, InternalServerError];   // 500エラー
+					else         [thisStatusCode, html] = [404, NotFound];              // 404エラー
+                });
+            } else [thisStatusCode, html] = [200, index];                   // 正常に動作した場合（200）
+
+            // ページをリターン
+            res.statusCode = thisStatusCode;
+            res.setHeader('Content-Type', 'text/html');
+			res.end(html);
+        });
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // '/updateInfo'にGETアクセスされた場合 admin/updateInfo.htmlを表示
+	} else if (req.url === '/updateInfo' && req.method === 'GET') {
+
+        // admin/updateInfo.htmlを読み込む
+        fs.readFile(docRoot + '/admin/updateInfo.html', {
+            encoding: 'utf8'
+        }, function(err_404, doc) {
+
+            if (err_404) {                                                  // admin/updateInfo.htmlの読み込みに失敗した場合
+                fs.readFile(docRoot + '/error/404.html', {
+                    encoding: 'utf8'
+                }, function(err_500, NotFound) {
+                    if (err_500) [thisStatusCode, html] = [500, InternalServerError];   // 500エラー
+                    else         [thisStatusCode, html] = [404, NotFound];              // 404エラー
+                });
+            } else [thisStatusCode, html] = [200, doc];                   // 正常に動作した場合（200）
+
+            // ページをリターン
+            res.statusCode = thisStatusCode;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(html);
+        });
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // '/'以外にGETアクセスされた場合 404エラーを返す
+    } else if (req.method === 'GET') {
+
+        // 404.htmlを読み込む
+        fs.readFile(docRoot + '/error/404.html', {
+            encoding: 'utf8'
+        }, function(err_500, NotFound) {
+            if (err_500) [thisStatusCode, html] = [500, InternalServerError];   // 500エラー
+            else         [thisStatusCode, html] = [404, NotFound];              // 404エラー
+
+            // ページをリターン
+            res.statusCode = thisStatusCode;
+            res.setHeader('Content-Type', 'text/html');
+			res.end(html);
         });
     }
-    
 });
 
-// --------------------------------------------------
-//                  Server Settings
-// --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
+//                                                  Server Settings
+// -----------------------------------------------------------------------------------------------------------------------
 // localhostの8080番ポートでサーバーを起動する
 server.listen(process.env.PORT || 8080);
+
+// ドキュメントルートの設定
+const docRoot = __dirname + '/www/html/';
 
 // Timeoutの設定
 const timeout_sec = 30;
